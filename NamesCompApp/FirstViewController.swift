81ecc4ad17f8a9617d7fcaed301c091c9215ff36
KarVarr <megatr9n@gmail.com>
2022-12-13 18:58:55 +0300

    added random method and progress bar

--- NamesCompApp/FirstViewController.swift
+++ NamesCompApp/FirstViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
     @IBOutlet weak var yourNameTF: UITextField!
     @IBOutlet weak var partherNameTF: UITextField!
     
@@ -16,14 +16,18 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
+    //MARK: - segue without id
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       guard let destinationVC = segue.destination as? ResultViewController else {return}
+        guard let destinationVC = segue.destination as? ResultViewController else {return}
         destinationVC.firstName = yourNameTF.text
         destinationVC.secondName = partherNameTF.text
     }
     
+    
+    //MARK: - segue with id
     @IBAction func resultButtonTapped() {
+        performSegue(withIdentifier: "goToResult", sender: nil)
     }
 }
 
--- NamesCompApp/ResultViewController.swift
+++ NamesCompApp/ResultViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
     
     @IBOutlet weak var namesLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
@@ -16,13 +16,22 @@ class ResultViewController: UIViewController {
     
     var firstName: String!
     var secondName: String!
+    var random = Int.random(in: 1...100)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        namesLabel.text = "\(firstName ?? "") and \(secondName ?? "")"
+        resultLabel.text = "\(random)%"
+        progressView.progress = Float(random) / 100
+        
+        progressView.transform = progressView.transform.scaledBy(x: 1, y: 4)
+        
     }
     
-
-   
-
+    
+    
+    
+    
+    
 }
